4882a93b4fedaf05d353b7c0fd5131a6c27657c9
Amanda <143744550+eimanda99@users.noreply.github.com>
2023-12-14 09:09:43 -0300

    Add files via upload

---
+++ jogo_bandeiras.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import random
+
+Brasil = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Flag_of_Brazil.svg/2560px-Flag_of_Brazil.svg.png"
+Estados_Unidos = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Flag_of_the_United_States.svg/1920px-Flag_of_the_United_States.svg.png"
+Franca = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Flag_of_France.svg/1920px-Flag_of_France.svg.png"
+Japao = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Flag_of_Japan.svg/2560px-Flag_of_Japan.svg.png"
+Alemanha = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Flag_of_Germany.svg/1920px-Flag_of_Germany.svg.png"
+
+bandeiras = [Brasil, Estados_Unidos, Franca, Japao, Alemanha]
+
+bandeira_atual = random.choice(bandeiras)
+
+
+st.title("Jogo de Adivinhação de Bandeiras")
+st.write("Tente adivinhar a bandeira do país!")
+
+while True: 
+  if st.button("Iniciar Jogo"):
+    st.image(bandeira_atual)
+    palpite = st.text_input("Digite um páis: ")
+    if palpite == bandeira_atual:
+      print("Acertou!")
+    else:
+      print("Errou") 
